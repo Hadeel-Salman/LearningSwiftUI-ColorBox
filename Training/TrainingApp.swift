e53728d674dac10558c1f0bcb384439775ce8c41
Hadeel <Hadeel.Salman.al@gmail.com>
2025-09-01 20:52:05 +0300

    Initial Commit

---
+++ Training/TrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingApp.swift
+//  Training
+//
+//  Created by Hadeel on 01/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct TrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
