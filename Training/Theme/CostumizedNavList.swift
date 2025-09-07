0e21f6a7b39979b0c4e1a3c81c4e0ace3f65ac62
Hadeel <Hadeel.Salman.al@gmail.com>
2025-09-07 23:29:58 +0300

    costumizable navigations

---
+++ Training/Theme/CostumizedNavList.swift
@@ -0,0 +1,18 @@
+//
+//  CostumizedNavList.swift
+//  Training
+//
+//  Created by Hadeel on 07/09/2025.
+//
+
+import SwiftUI
+
+struct CostumizedNavList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CostumizedNavList()
+}
