53cafc37f74910618391df38852d9a48082693f8
Hadeel <Hadeel.Salman.al@gmail.com>
2025-09-07 20:13:26 +0300

    adding navigations to the app

---
+++ Training/App/HomePageView.swift
@@ -0,0 +1,18 @@
+//
+//  HomePageView.swift
+//  Training
+//
+//  Created by Hadeel on 07/09/2025.
+//
+
+import SwiftUI
+
+struct HomePageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomePageView()
+}
---
+++ Training/ResizableCircle.swift
@@ -0,0 +1,18 @@
+//
+//  ResizableCircle.swift
+//  Training
+//
+//  Created by Hadeel on 06/09/2025.
+//
+
+import SwiftUI
+
+struct ResizableCircle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResizableCircle()
+}
