b10f8804004a246fb3e40c2c841197d2a74eef39
Hadeel <Hadeel.Salman.al@gmail.com>
2025-09-09 01:31:02 +0300

    practice: ObservableObject and @StateObject with a list view

---
+++ Training/App/FavFood.swift
@@ -0,0 +1,18 @@
+//
+//  FavFood.swift
+//  Training
+//
+//  Created by Hadeel on 09/09/2025.
+//
+
+import SwiftUI
+
+struct FavFood: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavFood()
+}
---
+++ Training/App/favFoodListView.swift
@@ -0,0 +1,18 @@
+//
+//  favFoodListView.swift
+//  Training
+//
+//  Created by Hadeel on 09/09/2025.
+//
+
+import SwiftUI
+
+struct favFoodListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    favFoodListView()
+}
