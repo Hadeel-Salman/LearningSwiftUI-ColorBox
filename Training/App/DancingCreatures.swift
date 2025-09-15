725e80a4b62573cbe6a73325d088e36b44bb1ac3
Hadeel <Hadeel.Salman.al@gmail.com>
2025-09-15 23:27:08 +0300

    being able to delete from a list of data

---
+++ Training/App/DancingCreatures.swift
@@ -0,0 +1,18 @@
+//
+//  DancingCreatures.swift
+//  Training
+//
+//  Created by Hadeel on 15/09/2025.
+//
+
+import SwiftUI
+
+struct DancingCreatures: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DancingCreatures()
+}
---
+++ Training/App/emojiPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  emojiPickerView.swift
+//  Training
+//
+//  Created by Hadeel on 15/09/2025.
+//
+
+import SwiftUI
+
+struct emojiPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    emojiPickerView()
+}
