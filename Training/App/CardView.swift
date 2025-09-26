77c0cbdd6571df554e8a0eb925a6ff0b9b6f3f70
Hadeel <Hadeel.Salman.al@gmail.com>
2025-09-26 23:52:11 +0300

    Name Picker Page

---
+++ Training/App/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  Training
+//
+//  Created by Hadeel on 26/09/2025.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
