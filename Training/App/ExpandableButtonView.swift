1981196fd85eb64e06f2a10a2fbb81aa20081c1f
Hadeel <Hadeel.Salman.al@gmail.com>
2025-09-28 22:28:35 +0300

    Added an ExpandableButtonView

---
+++ Training/App/ExpandableButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpandableButtonView.swift
+//  Training
+//
+//  Created by Hadeel on 28/09/2025.
+//
+
+import SwiftUI
+
+struct ExpandableButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpandableButtonView()
+}
