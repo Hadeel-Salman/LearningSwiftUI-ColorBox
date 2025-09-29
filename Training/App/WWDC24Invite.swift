107a586fb8d5a7eafca53fbb73938ab19753a444
Hadeel <Hadeel.Salman.al@gmail.com>
2025-09-30 01:10:10 +0300

    added an info button

---
+++ Training/App/WWDC24Invite.swift
@@ -0,0 +1,18 @@
+//
+//  WWDC24Invite.swift
+//  Training
+//
+//  Created by Hadeel on 29/09/2025.
+//
+
+import SwiftUI
+
+struct WWDC24Invite: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WWDC24Invite()
+}
