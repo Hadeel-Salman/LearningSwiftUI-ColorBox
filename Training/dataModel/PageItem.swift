45bb7df20030a42c5f6e6736d765930a157fe5f0
Hadeel <Hadeel.Salman.al@gmail.com>
2025-09-30 23:32:01 +0300

    Clean up HomePageView by introducing PageItem model to reduce code duplication

---
+++ Training/dataModel/PageItem.swift
@@ -0,0 +1,18 @@
+//
+//  PageItem.swift
+//  Training
+//
+//  Created by Hadeel on 30/09/2025.
+//
+
+import SwiftUI
+
+struct PageItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PageItem()
+}
