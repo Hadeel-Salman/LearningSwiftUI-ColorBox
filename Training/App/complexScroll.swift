a571d982326a3937a13f1da477ee8f789bd9ea4f
Hadeel <Hadeel.Salman.al@gmail.com>
2025-10-06 23:56:30 +0300

    learning complex Scroll

---
+++ Training/App/complexScroll.swift
@@ -0,0 +1,18 @@
+//
+//  complexScroll.swift
+//  Training
+//
+//  Created by Hadeel on 06/10/2025.
+//
+
+import SwiftUI
+
+struct complexScroll: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    complexScroll()
+}
---
+++ Training/dataModel/Expense.swift
@@ -0,0 +1,8 @@
+//
+//  Expense.swift
+//  Training
+//
+//  Created by Hadeel on 06/10/2025.
+//
+
+import Foundation
